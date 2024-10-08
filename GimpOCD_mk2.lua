c12941b87beba604d3e5ae06ae814621eb886a2a
Gimpeh <thatgimpehfellow@gmail.com>
2024-10-08 05:31:40 -0500

    added initial onclick

--- GimpOCD_mk2.lua
+++ GimpOCD_mk2.lua
@@ -14,6 +14,23 @@ component.glasses.removeAll()
 
 players = {}
 
+glasses_display.init()
+
+local function onClick(eventName, address, player, x, y, button)
+    print("Handling onClick event")
+    local suc, err = pcall(function()
+        if eventName == "hud_click" and button == 0 then
+            print("Left-click detected")
+            glasses_display.onClick(eventName, address, player, x, y, button)
+        elseif eventName == "hud_click" and button == 1 then
+            print("Right-click detected")
+            glasses_display.onClickRight(eventName, address, player, x, y, button)
+        end     
+    end)
+end
+
+event.listen("hud_click", onClick)
+
 while true do
     os.sleep(5)
 end
