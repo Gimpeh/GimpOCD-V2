f2135147f0de4b0c747149374a1c76995a8ca423
Gimpeh <thatgimpehfellow@gmail.com>
2024-10-08 03:24:30 -0500

    nope, there will be a at least 2 more. 1 for prints, 1 for pcalls

--- GimpOCD_mk2.lua
+++ GimpOCD_mk2.lua
@@ -1,7 +1,13 @@
+package.path = package.path .. ";/GimpOCD-V2/?.lua"
 local event = require("event")
 component = require("component")
 local glasses_display = require("displays.glasses_display")
 
+print("removing all widgets")
 component.glasses.removeAll()
 
 players = {}
+
+while true do
+    os.sleep(5)
+end
