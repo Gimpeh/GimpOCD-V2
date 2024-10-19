b80819dc99e9281f4ed53610a659eb77109b6306
Gimpeh <thatgimpehfellow@gmail.com>
2024-10-19 03:45:08 -0500

    fix modules crap

--- displays/modules/modules.lua
+++ displays/modules/modules.lua
@@ -2,7 +2,7 @@ print("modules.lua loaded and processing")
 
 --local cpu_director = require("displays.modules.cpu_controller.cpu_director").init
 local item_overseer = require("displays.modules.item_overseer.item_overseer").init
---local machine_controller = require("displays.modules.machine_overseer.machine_controller").init
+local machine_controller = require("displays.modules.machine_controller.machine_controller").init
 local power_overseer = require("displays.modules.power_overseer/power_overseer").init
 --local robot_director = require("displays.modules.robot_director/robot_director").init
 --local text_editor = require("displays.modules.text_editor/text_editor").init
@@ -13,7 +13,7 @@ local function new()
     local available_modules = {
         --me_cpu_controller = me_cpu_controller,
         item_overseer = item_overseer,
-        --machine_overseer = machine_overseer,
+        machine_controller = machine_controller,
         power_overseer = power_overseer,
         --robot_director = robot_director,
         --text_editor = text_editor
