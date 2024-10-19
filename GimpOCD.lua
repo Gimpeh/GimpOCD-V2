3ca89f780d9fac733f8c1a2a7d70931030ade9a9
Gimpeh <thatgimpehfellow@gmail.com>
2024-10-19 03:37:21 -0500

    derp

--- GimpOCD.lua
+++ GimpOCD.lua
@@ -2,7 +2,7 @@ local shell = require("shell")
 
 local args, options = shell.parse(...)
 
-if args and args[1] and type(args[1]) == "number" then
+if args and args[1] then
     os.execute("/home/GimpOCD-V2/GimpOCD_mk2.lua " .. tostring(args[1]))
 else
     os.execute("/home/GimpOCD-V2/GimpOCD_mk2.lua")
